66873808bd70b30b9717ffdb6ea0520416c5df91
Matthew <78713317+mreed4@users.noreply.github.com>
2022-11-12 13:27:41 -0800

    Continue building TrackList

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,7 @@
+import "./TrackList.css";
+
+function TrackList() {
+  return <div className="TrackList">{/* You will add a map method that renders a set of Track components */}</div>;
+}
+
+export default TrackList;
