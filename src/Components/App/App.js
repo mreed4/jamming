8389b92b1bf035463889e364451fbf98ac5311ff
Matthew <78713317+mreed4@users.noreply.github.com>
2022-11-12 13:00:46 -0800

    New content in App, add background image

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        Ja<span className="highlight">mmm</span>ing
+      </h1>
+      <div className="App">
+        <div className="App-playlist"></div>
+      </div>
     </div>
   );
 }
