88ba59ad14143517c036f693ce3bd6d4366e2560
Matthew <78713317+mreed4@users.noreply.github.com>
2022-11-12 13:08:29 -0800

    Continu building SearchBar; App.js: comment out import

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,12 @@
+import "./SearchBar.css";
+
+function SearchBar() {
+  return (
+    <div className="SearchBar">
+      <input placeholder="Enter A Song, Album, or Artist" />
+      <button className="SearchButton">SEARCH</button>
+    </div>
+  );
+}
+
+export default SearchBar;
