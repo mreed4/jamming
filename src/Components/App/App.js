d6b3fa6299505adcd15766ede088dd22f66d32b1
Matthew <78713317+mreed4@users.noreply.github.com>
2022-11-19 14:15:09 -0800

    Build 'change playlist name' functionality

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -25,6 +25,7 @@ class App extends React.Component {
 
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   addTrack(track) {
@@ -43,6 +44,10 @@ class App extends React.Component {
     tracks = tracks.filter((currentTrack) => currentTrack.id !== track.id);
 
     this.setState({ playlistTracks: tracks });
+  }
+
+  updatePlaylistName(name) {
+    this.setState({ playlistName: name });
   }
 
   render() {
