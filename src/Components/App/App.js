54955adcf88863e0d2caf60f8571fa57f90af5c2
Matthew <78713317+mreed4@users.noreply.github.com>
2022-11-19 12:58:07 -0800

    PLaylist.js begins to work

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -15,6 +15,12 @@ class App extends React.Component {
         { name: "name2", artist: "artist2", album: "album2", id: 2 },
         { name: "name3", artist: "artist3", album: "album3", id: 3 },
       ],
+      playlistName: "My Playlist",
+      playlistTracks: [
+        { name: "playlistName1", artist: "playlistArtist1", album: "playlistAlbum1", id: 4 },
+        { name: "playlistName2", artist: "playlistArtist2", album: "playlistAlbum2", id: 5 },
+        { name: "playlistName3", artist: "playlistArtist3", album: "playlistAlbum3", id: 6 },
+      ],
     };
   }
 
@@ -28,7 +34,7 @@ class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} />
-            <Playlist />
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
           </div>
         </div>
       </div>
--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -1,14 +1,14 @@
 import "./Playlist.css";
 
 import React from "react";
-// import TrackList from "../TrackList/TrackList";
+import TrackList from "../TrackList/TrackList";
 
 class Playlist extends React.Component {
   render() {
     return (
       <div className="Playlist">
         <input defaultValue={"New Playlist"} />
-        {/* <TrackList /> */}
+        <TrackList tracks={this.props.playlistTracks} />
         <button className="Playlist-save">SAVE TO SPOTIFY</button>
       </div>
     );
