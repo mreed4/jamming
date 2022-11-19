7c4b12d939b42f3dc9c1cb61305e0415015a3b14
Matthew <78713317+mreed4@users.noreply.github.com>
2022-11-19 15:36:52 -0800

    Continue to build out Spotify API stuff

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,3 +1,22 @@
-const Spotify = {};
+let accessToken;
+
+const Spotify = {
+  getAccessToken() {
+    if (accessToken) {
+      return accessToken;
+    }
+
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+    if (accessTokenMatch && expiresMatch) {
+      accessToken = accessTokenMatch[1];
+      const expiresIn = Number(expiresMatch[1]);
+      window.setTimeout(() => (accessToken = ""), expiresIn * 1000);
+      window.history.pushState("Access Token", null, "/");
+      return accessToken;
+    }
+  },
+};
 
 export default Spotify;
