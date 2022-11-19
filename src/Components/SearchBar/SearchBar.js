6afce029e8ba50f613f985cddd64d5b1d3e93cd7
Matthew <78713317+mreed4@users.noreply.github.com>
2022-11-19 15:05:54 -0800

    Continue building 'search' functionality

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -7,16 +7,21 @@ class SearchBar extends React.Component {
     super(props);
 
     this.search = this.search.bind(this);
+    this.handleTermChange = this.handleTermChange.bind(this);
   }
 
   search() {
     this.props.onSearch(this.state.searchTerm);
   }
 
+  handleTermChange(e) {
+    this.setState({ term: e.target.value });
+  }
+
   render() {
     return (
       <div className="SearchBar">
-        <input placeholder="Enter A Song, Album, or Artist" />
+        <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} />
         <button className="SearchButton">SEARCH</button>
       </div>
     );
