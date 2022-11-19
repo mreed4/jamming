b78ae3c11533be7e7031377df0b0c4fe24d9de5d
Matthew <78713317+mreed4@users.noreply.github.com>
2022-11-19 13:06:29 -0800

    Begin building 'add track' functionality

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -22,6 +22,18 @@ class App extends React.Component {
         { name: "playlistName3", artist: "playlistArtist3", album: "playlistAlbum3", id: 6 },
       ],
     };
+    this.addTrack = this.addTrack.bind(this);
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+
+    if (tracks.find((savedTrack) => savedTrack.id === track.id)) {
+      return;
+    }
+
+    tracks.push(track);
+    this.setState({ playlistTracks: tracks });
   }
 
   render() {
@@ -33,7 +45,7 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} />
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
             <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
           </div>
         </div>
