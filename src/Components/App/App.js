4866beb389db871ee3a92c82af894c38fa5ebb79
Matthew <78713317+mreed4@users.noreply.github.com>
2022-11-20 10:50:50 -0800

    Continue building the savePLaylist functionality

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -11,11 +11,7 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      searchResults: [
-        { name: "name1", artist: "artist1", album: "album1", id: 1 },
-        { name: "name2", artist: "artist2", album: "album2", id: 2 },
-        { name: "name3", artist: "artist3", album: "album3", id: 3 },
-      ],
+      searchResults: [],
       playlistName: "My Playlist",
       playlistTracks: [
         { name: "playlistName1", artist: "playlistArtist1", album: "playlistAlbum1", id: 4 },
@@ -56,6 +52,9 @@ class App extends React.Component {
   savePlaylist() {
     // alert("Correctly linked");
     const trackUris = this.state.playlistTracks.map((track) => track.uri);
+    Spotify.savePlaylist(this.state.playlistName, trackUris).then(() => {
+      this.setState({ playlistName: "New PLaylist", playlistTracks: [] });
+    });
   }
 
   search(term) {
--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -75,6 +75,11 @@ const Spotify = {
           .then((response) => response.json())
           .then((jsonResponse) => {
             const playlistId = jsonResponse.id;
+            return fetch(`https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks`, {
+              headers: headers,
+              method: "POST",
+              body: JSON.stringify({ uris: trackUris }),
+            });
           });
       });
   },
