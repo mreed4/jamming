f5b38136fd33e3fedd14136be522be6579987636
Matthew <78713317+mreed4@users.noreply.github.com>
2022-11-20 10:32:28 -0800

    Continue building the savePLaylist functionality

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -54,6 +54,20 @@ const Spotify = {
     if (!playlistName || !trackUris.length) {
       return;
     }
+
+    const accessToken = Spotify.getAccessToken();
+    const headers = {
+      Authorization: `Bearer ${accessToken}`,
+    };
+    let userId;
+
+    return fetch("https://api.spotify.com/v1/me", { headers: headers })
+      .then((response) => {
+        return response.json();
+      })
+      .then((jsonResponse) => {
+        userId = jsonResponse.id;
+      });
   },
 };
 
