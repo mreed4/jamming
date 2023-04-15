c4557eb41be5a2a80f62110abf148e9bb26bb9cf
Matthew <78713317+mreed4@users.noreply.github.com>
2023-04-15 14:14:02 -0700

    Fix broken imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./Components/App/App";
-import "./assets/styles/reset.css";
-import "./assets/styles/index.css";
+import App from "./Components/App";
+import "./assets/css/reset.css";
+import "./assets/css/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
