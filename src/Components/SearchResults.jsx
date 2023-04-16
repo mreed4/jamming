4a23329ca9c2c3ed92ff97f39ecf0a763f2b2818
Matthew <78713317+mreed4@users.noreply.github.com>
2023-04-16 11:25:59 -0700

    Convert to function component

--- src/Components/SearchResults.jsx
+++ src/Components/SearchResults.jsx
@@ -1,15 +1,28 @@
 import TrackList from "./TrackList";
-import React from "react";
+import { useContext } from "react";
+import { AppContext } from "../AppContext";
 
-class SearchResults extends React.Component {
-  render() {
-    return (
+export default function SearchResults() {
+  const { searchResults } = useContext(AppContext);
+
+  return (
+    Object.keys(searchResults).length > 0 && (
       <div>
         <h2>Results</h2>
-        <TrackList tracks={this.props.searchResults} onAdd={this.props.onAdd} isRemoval={false} />
+        <h3>{Object.keys(searchResults)[0][0].toUpperCase() + Object.keys(searchResults)[0].slice(1)}</h3>
+        <ol>
+          {searchResults.tracks.items.map((track) => {
+            return (
+              <li key={track.uri}>
+                <img src={track.album.images[2].url} alt={track.name} />
+                {track.name} <br />
+                {track.artists.map((artist) => artist.name).join(", ")} <br />
+                {track.album.name} <br />
+              </li>
+            );
+          })}
+        </ol>
       </div>
-    );
-  }
+    )
+  );
 }
-
-export default SearchResults;
