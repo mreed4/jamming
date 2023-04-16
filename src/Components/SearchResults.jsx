5631df3781e14ed5f43919798745c8d567adf4e9
Matthew <78713317+mreed4@users.noreply.github.com>
2023-04-16 15:02:59 -0700

    More destructuring

--- src/Components/SearchResults.jsx
+++ src/Components/SearchResults.jsx
@@ -4,6 +4,7 @@ import { AppContext } from "../AppContext";
 
 export default function SearchResults() {
   const { searchResults } = useContext(AppContext);
+  const { albums, artists, tracks } = searchResults;
 
   return (
     Object.keys(searchResults).length > 0 && (
@@ -11,10 +12,10 @@ export default function SearchResults() {
         <h2>Results</h2>
         <h3>{Object.keys(searchResults)[0][0].toUpperCase() + Object.keys(searchResults)[0].slice(1)}</h3>
         <ol>
-          {searchResults.tracks.items.map((track) => {
+          {tracks.items.map((track) => {
             return (
               <li key={track.uri}>
-                <img src={track.album.images[2].url} alt={track.name} />
+                <img src={track.album.images[1].url} alt={track.name} className="album" />
                 {track.name} <br />
                 {track.artists.map((artist) => artist.name).join(", ")} <br />
                 {track.album.name} <br />
