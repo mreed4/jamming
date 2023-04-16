3f59c37422ddf8f566887e46df997eb40f47efb2
Matthew <78713317+mreed4@users.noreply.github.com>
2023-04-16 15:02:06 -0700

    Convert to function

--- src/Components/Track.jsx
+++ src/Components/Track.jsx
@@ -1,14 +1,5 @@
-import React from "react";
-
-class Track extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.addTrack = this.addTrack.bind(this);
-    this.removeTrack = this.removeTrack.bind(this);
-  }
-
-  renderAction() {
+export default function Track() {
+  function renderAction() {
     if (this.props.isRemoval) {
       return <button onClick={this.removeTrack}>-</button>;
     } else {
@@ -16,28 +7,24 @@ class Track extends React.Component {
     }
   }
 
-  addTrack() {
+  function addTrack() {
     this.props.onAdd(this.props.track);
   }
 
-  removeTrack() {
+  function removeTrack() {
     this.props.onRemove(this.props.track);
   }
 
-  render() {
-    return (
+  return (
+    <div>
+      <img src={this.props.track.albumArt} />
       <div>
-        <img src={this.props.track.albumArt} />
-        <div>
-          <h3>{this.props.track.name}</h3>
-          <p>
-            {this.props.track.artist} | {this.props.track.album}
-          </p>
-        </div>
-        {this.renderAction()}
+        <h3>{this.props.track.name}</h3>
+        <p>
+          {this.props.track.artist} | {this.props.track.album}
+        </p>
       </div>
-    );
-  }
+      {this.renderAction()}
+    </div>
+  );
 }
-
-export default Track;
