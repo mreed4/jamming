93aacec73e75c96c447b4d95857782830674e158
Matthew <78713317+mreed4@users.noreply.github.com>
2023-04-16 11:02:15 -0700

    Convert to function component

--- src/Components/TrackList.jsx
+++ src/Components/TrackList.jsx
@@ -1,18 +1,11 @@
 import Track from "./Track";
-import React from "react";
 
-class TrackList extends React.Component {
-  render() {
-    return (
-      <div>
-        {this.props.tracks.map((track) => {
-          return (
-            <Track track={track} key={track.id} onAdd={this.props.onAdd} onRemove={this.props.onRemove} isRemoval={this.props.isRemoval} />
-          );
-        })}
-      </div>
-    );
-  }
+export default function TrackList({ tracks }) {
+  return (
+    <div>
+      {tracks.map((track) => {
+        return <Track track={track} key={track.id} onAdd={props.onAdd} onRemove={this.props.onRemove} isRemoval={this.props.isRemoval} />;
+      })}
+    </div>
+  );
 }
-
-export default TrackList;
