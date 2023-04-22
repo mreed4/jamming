033b57c0f0d42014cec577897269be4a37e6e47c
Matthew <78713317+mreed4@users.noreply.github.com>
2023-04-21 19:50:38 -0700

    Searching will navigate to results

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,15 +1,24 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { AppContext } from "../AppContext";
 
 export default function SearchBar() {
   const { handleSearchTermChange, handleFormSubmit, searchTerm } = useContext(AppContext);
 
+  const navigate = useNavigate();
+
+  function handleSearch(e) {
+    e.preventDefault();
+    handleFormSubmit();
+    navigate("/results");
+  }
+
   return (
-    <div>
-      <form onSubmit={handleFormSubmit}>
+    <section id="search-bar">
+      <form onSubmit={handleSearch}>
         <input placeholder="Enter A Song, Album, or Artist" value={searchTerm} onChange={handleSearchTermChange} />
         <button type="submit">Search</button>
       </form>
-    </div>
+    </section>
   );
 }
