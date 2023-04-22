f2bc4b52b6e37d0e7295c6e2a587ed5ae9776bb3
Matthew <78713317+mreed4@users.noreply.github.com>
2023-04-21 19:45:10 -0700

    Remove hardcoding

--- src/Components/SearchResults.jsx
+++ src/Components/SearchResults.jsx
@@ -4,16 +4,17 @@ import { AppContext } from "../AppContext";
 
 export default function SearchResults() {
   const { searchResults } = useContext(AppContext);
-  const { albums, artists, tracks } = searchResults;
 
   return (
     Object.keys(searchResults).length > 0 && (
       <section id="search-results">
         <h2>Results</h2>
         <div id="item-lists">
-          <ItemList items={tracks.items} itemType="tracks" />
-          <ItemList items={artists.items} itemType="artists" />
-          <ItemList items={albums.items} itemType="albums" />
+          {Object.keys(searchResults)
+            .reverse()
+            .map((key) => {
+              return <ItemList key={key} items={searchResults[key].items} itemType={key} />;
+            })}
         </div>
       </section>
     )
