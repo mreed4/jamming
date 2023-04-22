8fdebd1fc609325bf406fd0e422d50ed001b240a
Matthew <78713317+mreed4@users.noreply.github.com>
2023-04-21 19:45:35 -0700

    Add routing

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,4 +1,5 @@
 import { AppProvider } from "../AppContext";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SearchBar from "./SearchBar";
 import SearchResults from "./SearchResults";
 import Playlist from "./Playlist";
@@ -6,12 +7,17 @@ import Playlist from "./Playlist";
 export default function App() {
   return (
     <AppProvider>
-      <main>
-        <h1>Audiofile</h1>
-        <SearchBar />
-        <SearchResults />
-        {/* <Playlist /> */}
-      </main>
+      <Router>
+        <main>
+          <h1>Audiofile</h1>
+          <Routes>
+            <Route path="/" element={<SearchBar />} />
+            <Route path="/results" element={<SearchResults />} />
+            {/* <Route path="/playlist" element={<Playlist />} /> */}
+            <Route path="*" element={<h1>404</h1>} />
+          </Routes>
+        </main>
+      </Router>
     </AppProvider>
   );
 }
