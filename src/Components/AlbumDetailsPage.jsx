38e8f0d3009c6236d5282dbbf16d93f5edd37998
Matthew <78713317+mreed4@users.noreply.github.com>
2023-04-30 16:55:03 -0700

    Conditionally render album details

--- src/Components/AlbumDetailsPage.jsx
+++ src/Components/AlbumDetailsPage.jsx
@@ -1,33 +1,43 @@
-import { useLocation, Link, useNavigate } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
 import { useContext, useEffect } from "react";
 import { AppContext } from "../AppContext";
 import Spotify from "../util/Spotify";
 
 export default function AlbumDetailsPage() {
   const location = useLocation();
-  const { state: album } = location;
-  const navigate = useNavigate();
-  const goBack = () => navigate(-1);
+  const { state } = location;
 
   const { parseArtists, albumTracks, setAlbumTracks, parseTrackTitle, addLeadingZero, toMinutesAndSeconds } = useContext(AppContext);
-  // console.log(album);
 
   useEffect(() => {
     setAlbumTracks([]);
-    Spotify.getAlbumTracks(album.id).then((album) => {
-      console.log(album);
-      setAlbumTracks(album.items);
-    });
+    if (!state) return;
+    if (state.type === "album") {
+      Spotify.getAlbumTracks(state.id)
+        .then((state) => {
+          setAlbumTracks(state.items);
+        })
+        .catch((err) => console.error(err));
+    }
+    if (state.type === "track") {
+      Spotify.getAlbumTracks(state.album.id)
+        .then((state) => {
+          setAlbumTracks(state.items);
+        })
+        .catch((err) => console.error(err));
+    }
   }, []);
 
   return (
     <section id="album-details">
-      <button onClick={goBack}>Back</button>
+      <Link to="/results/tracks" className="back-button">
+        &larr; Back
+      </Link>
       <div className="album-image-and-tracks">
         <div>
-          <img src={album.images[0].url} className="album-image" />
-          <h2>{album.name}</h2>
-          <h3>{parseArtists(album.artists)}</h3>
+          <img src={state.type === "track" ? state.album.images[0].url : state.images[0].url} className="album-image" />
+          <h2>{state.name}</h2>
+          <h3>{parseArtists(state.artists)}</h3>
         </div>
         <ol className="album-track-list">
           {albumTracks.map((track, i) => {
