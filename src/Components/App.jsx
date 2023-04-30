d3b2c3d899bb4e3a6e7701b0b9a5d33ce00bc9a5
Matthew <78713317+mreed4@users.noreply.github.com>
2023-04-30 16:06:53 -0700

    Implement ScrollToTop

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -6,6 +6,7 @@ import SearchPage from "./SearchPage";
 import ResultsPage from "./ResultsPage";
 import LoadingPage from "./LoadingPage";
 import AlbumDetailsPage from "./AlbumDetailsPage";
+import ScrollToTop from "./ScrollToTop";
 import Playlist from "./Playlist";
 
 export default function App() {
@@ -13,19 +14,20 @@ export default function App() {
 
   return (
     <Router>
-      <main>
-        <h1 id="site-title">
-          <Link to="/">Audiophile</Link>
-        </h1>
-        <hr />
-        <Routes>
-          <Route path="/" element={<SearchPage />} />
-          {resultsArray.length > 0 ? (
-            <Route path="/results" element={<ResultsPage />}>
-              {resultsArray.reverse().map((key) => {
-                return <Route key={key} path={key} element={<ItemList items={searchResults[key].items} itemTypes={key} />} />;
-              })}
-              {/* 
+      <ScrollToTop>
+        <main>
+          <h1 id="site-title">
+            <Link to="/">Audiophile</Link>
+          </h1>
+          <hr />
+          <Routes>
+            <Route path="/" element={<SearchPage />} />
+            {resultsArray.length > 0 ? (
+              <Route path="/results" element={<ResultsPage />}>
+                {resultsArray.reverse().map((key) => {
+                  return <Route key={key} path={key} element={<ItemList items={searchResults[key].items} itemTypes={key} />} />;
+                })}
+                {/* 
 
               The above array.map is the equivalent of hardcoding the following:
 
@@ -34,16 +36,17 @@ export default function App() {
               <Route path="albums" element={<ItemList items={searchResults.albums.items} itemTypes="albums" />} />
 
                */}
-            </Route>
-          ) : (
-            <Route path="/results/tracks" element={<LoadingPage />} />
-          )}
-          {/* <Route path="/playlist" element={<Playlist />} /> */}
+              </Route>
+            ) : (
+              <Route path="/results/tracks" element={<LoadingPage />} />
+            )}
+            {/* <Route path="/playlist" element={<Playlist />} /> */}
 
-          <Route path="/album/:artist/:name" element={<AlbumDetailsPage />} />
-          <Route path="*" element={<h2>404</h2>} />
-        </Routes>
-      </main>
+            <Route path="/album/:artist/:name" element={<AlbumDetailsPage />} />
+            <Route path="*" element={<h2>404</h2>} />
+          </Routes>
+        </main>
+      </ScrollToTop>
     </Router>
   );
 }
