b290818375c88630468871650e1241228b82ba81
Matthew <78713317+mreed4@users.noreply.github.com>
2023-05-01 20:48:08 -0700

    Clean up

--- src/Components/ResultsPage.jsx
+++ src/Components/ResultsPage.jsx
@@ -1,6 +1,4 @@
-import ItemList from "./ItemList";
-import { NavLink, Link, Outlet } from "react-router-dom";
-import SearchForm from "./SearchForm";
+import { NavLink, Outlet } from "react-router-dom";
 import { useContext } from "react";
 import { AppContext } from "../AppContext";
 
@@ -8,29 +6,25 @@ export default function ResultsPage() {
   const { getClassName, resultsArray, searchTermPersist, toProperCase } = useContext(AppContext);
 
   return (
-    <>
-      <section id="results-page">
-        {/* <SearchForm /> */}
-        <h2 className="italic">Results for "{searchTermPersist}"</h2>
-        {/* <Link to="/">Back</Link> */}
-        <nav id="results-nav">
-          <ul>
-            {resultsArray.length > 0 &&
-              resultsArray.map((key) => {
-                return (
-                  <li key={key}>
-                    <NavLink to={key} className={getClassName}>
-                      {toProperCase(key)}
-                    </NavLink>
-                  </li>
-                );
-              })}
-          </ul>
-        </nav>
-        <div id="item-lists">
-          <Outlet />
-        </div>
-      </section>
-    </>
+    <section id="results-page">
+      <h2 className="italic">Results for "{searchTermPersist}"</h2>
+      <nav id="results-nav">
+        <ul>
+          {resultsArray.length > 0 &&
+            resultsArray.map((key) => {
+              return (
+                <li key={key}>
+                  <NavLink to={key} className={getClassName}>
+                    {toProperCase(key)}
+                  </NavLink>
+                </li>
+              );
+            })}
+        </ul>
+      </nav>
+      <div id="item-lists">
+        <Outlet />
+      </div>
+    </section>
   );
 }
