9f37749412af63e37a9ac08f08b5460276f7ab83
Matthew <78713317+mreed4@users.noreply.github.com>
2023-05-03 20:04:46 -0700

    Remove console logs

--- src/Components/ItemList.jsx
+++ src/Components/ItemList.jsx
@@ -1,12 +1,10 @@
 import Items from "./Items";
 
 export default function ItemList({ items, itemTypes }) {
-  // console.log(items);
   return (
     <section id={`${itemTypes}-list`} className="item-list">
       <ol>
         {items.map((item) => {
-          // console.log(item);
           const track = <Items item={item} itemType="track" />;
           const artist = <Items item={item} itemType="artist" />;
           const album = <Items item={item} itemType="album" />;
