737699a423a1471356dfcec64cc6e434065f85a7
Matthew <78713317+mreed4@users.noreply.github.com>
2023-05-05 21:22:20 -0700

    Further modularize

--- src/Components/AlbumItem.jsx
+++ src/Components/AlbumItem.jsx
@@ -1,27 +1,14 @@
 import { useContext } from "react";
 import { AppContext } from "../AppContext";
 import { Link } from "react-router-dom";
+import AlbumItemContents from "./AlbumItemContents";
 
 export default function AlbumItem({ album, itemType }) {
-  const { parseArtists, toKebabCase, albumIsSingleOrCompilation, parseAlbumTitle, placeholderImage } = useContext(AppContext);
-
-  const src = album.images[1]?.url ?? placeholderImage;
+  const { toKebabCase } = useContext(AppContext);
 
   return (
     <Link to={`/album/${toKebabCase(album.artists[0].name)}/${toKebabCase(album.name)}`} state={album}>
-      <div className="album-item list-image">
-        <div className="album-info">
-          <div>
-            <span className="album-title">{parseAlbumTitle(album.name)}</span>
-            <span>{albumIsSingleOrCompilation(album)}</span>
-          </div>
-          <div>
-            <span className={album.artists.length > 1 ? "multi-artist" : ""}>{parseArtists(album.artists, itemType)}</span>
-            <span className="italic">{album.release_date.slice(0, 4)}</span>
-          </div>
-        </div>
-        <img src={src} className="album-image" alt={album.name} title={album.name} />
-      </div>
+      <AlbumItemContents album={album} itemType={itemType} />
     </Link>
   );
 }
---
+++ src/Components/AlbumItemContents.jsx
@@ -0,0 +1,24 @@
+import { useContext } from "react";
+import { AppContext } from "../AppContext";
+
+export default function AlbumItemContents({ album, itemType }) {
+  const { parseArtists, albumIsSingleOrCompilation, parseAlbumTitle, placeholderImage } = useContext(AppContext);
+
+  const src = album.images[1]?.url ?? placeholderImage;
+
+  return (
+    <div className="album-item list-image">
+      <div className="album-info">
+        <div>
+          <span className="album-title">{parseAlbumTitle(album.name)}</span>
+          <span>{albumIsSingleOrCompilation(album)}</span>
+        </div>
+        <div>
+          <span className={album.artists.length > 1 ? "multi-artist" : ""}>{parseArtists(album.artists, itemType)}</span>
+          <span className="italic">{album.release_date.slice(0, 4)}</span>
+        </div>
+      </div>
+      <img src={src} className="album-image" alt={album.name} title={album.name} />
+    </div>
+  );
+}
